2f4eb8ff6610af5d64d11097ea7bdf3d3bd2fd70
Ria Legaspi <riaveluz@gmail.com>
2023-07-21 16:22:23 -0700

    delete session

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
   post "/signin", to: "sessions#create"
   get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
